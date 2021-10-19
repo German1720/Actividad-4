a081728316fa3f8de0e2664c211c63fadae31594
German1720 <germanvillalba3@gmail.com>
2021-10-19 16:55:23 -0500

    actualizacion

--- funciones.js
+++ funciones.js
@@ -1,46 +1,28 @@
-/*function sumar()
-{
-  const $total = document.getElementById('total');
-  let subtotal = 0;
-  [ ...document.getElementsByClassName( "monto" ) ].forEach( function ( element ) {
-    if(element.value !== '') {
-      subtotal += parseFloat(element.value);
-    }
-  });
-  $total.value =  MediaStreamAudioDestinationNode; 
-
-  
-  /*function sumar(){
-      const total = document.getElementById("total")
-      let total= " "; 
-    [ document.getElementsByTagName('monto')].forEach( function (element ) {
-        if(element.value !== ''){
-            total+=parseFloat(element.value);
-        };
-        
-        
-    });
-  }
-  $total.value=MediaStreamAudioDestinationNode;*/
- 
-  function sumar() {
-
-  var total = 0;
-
-  $(".monto").each(function() {
-
-    if (isNaN(parseFloat($(this).val()))) {
-
-      total += 0;
-
-    } else {
-
-      total += parseFloat($(this).val());
-
-    }
-
-  });
-
-  document.getElementById('spTotal').innerHTML = total;
-
+function limpiar(){
+  document.getElementById('formulario').reset();
 }
+
+function Sumar(){
+  var a=parseInt(document.getElementById('Criterio1').value);
+  var b=parseInt(document.getElementById('Criterio2').value);
+  //alert(a+b); 
+  document.getElementById('Resultado').innerHTML = a+b; 
+}
+/*function Restar(){
+  var a=parseInt(document.getElementById('Criterio1').value);
+  var b=parseInt(document.getElementById('Criterio2').value);
+  //alert(a+b); 
+  document.getElementById('Resultado').innerHTML = a-b; 
+}
+function Multiplicar(){
+  var a=parseInt(document.getElementById('Criterio1').value);
+  var b=parseInt(document.getElementById('Criterio2').value);
+  //alert(a+b); 
+  document.getElementById('Resultado').innerHTML = a*b; 
+}
+function Dividir(){
+  var a=parseInt(document.getElementById('Criterio1').value);
+  var b=parseInt(document.getElementById('Criterio2').value);
+  //alert(a+b); 
+  document.getElementById('Resultado').innerHTML = a/b; 
+}*/
